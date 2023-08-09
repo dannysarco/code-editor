554468ea6f07bcd462b47b6877d14e1e3e1af0c4
Danny Sarco <dsarco@gmail.com>
2023-08-09 18:26:11 -0400

    Set initial value prop

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -65,7 +65,7 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor initialValue="const a = 1;" />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
