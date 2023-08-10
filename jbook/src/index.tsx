b15843af1266e74704ac9188e0d539306e0a8bf9
Danny Sarco <dsarco@gmail.com>
2023-08-10 14:14:52 -0400

    Added handling for editor change events

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -65,7 +65,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor initialValue="const a = 1;" />
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
