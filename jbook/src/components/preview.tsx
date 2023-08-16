36f12d290e98cd28ab9a568ee764810407f8d852
Danny Sarco <dsarco@gmail.com>
2023-08-16 17:09:04 -0400

    Adding more resizing to screen components

--- jbook/src/components/preview.tsx
+++ jbook/src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import { useRef, useEffect } from 'react';
 
 interface PreviewProps {
@@ -6,7 +7,9 @@ interface PreviewProps {
 
 const html = `
     <html>
-      <head></head>
+    <head>
+    <style>html { background-color: white; }</style>
+  </head>
       <body>
         <div id="root"></div>
         <script>
@@ -33,13 +36,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-    style={{ backgroundColor: 'white' }}
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
