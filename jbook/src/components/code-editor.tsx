7d7f65606c24d26a09a96a5780a2745fd28e6044
Danny Sarco <dsarco@gmail.com>
2023-08-17 18:56:15 -0400

    Adding immer and binding React with Redux

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,12 +1,11 @@
 import './code-editor.css';
 import './syntax.css';
-import { useRef } from "react";
-import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
-import prettier from "prettier";
-import parser from "prettier/parser-babel";
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 import codeShift from 'jscodeshift';
 import Highlighter from 'monaco-jsx-highlighter';
-
 
 interface CodeEditorProps {
   initialValue: string;
