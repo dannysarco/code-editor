c0480be1f7ceb68c491c2c4f9d568368c43787b0
Danny Sarco <dsarco@gmail.com>
2023-08-25 15:15:01 -0400

    First part of adding a local only api.

--- jbook/packages/cli/node_modules/local-api/dist/index.js
+++ jbook/packages/cli/node_modules/local-api/dist/index.js
@@ -1,9 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
-    console.log("serving traffic on port", port);
-    console.log("saving/fetching cells from", filename);
-    console.log("that file is in dir", dir);
+    const app = (0, express_1.default)();
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: "http://localhost:3000",
+        ws: true,
+        logLevel: "silent",
+    }));
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on("error", reject);
+    });
 };
 exports.serve = serve;
