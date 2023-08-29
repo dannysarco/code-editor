6101a5ad84156e75137cf3e4655fb3f78b8ca324
Danny Sarco <dsarco@gmail.com>
2023-08-29 19:56:08 -0400

    Bug fixes

--- jbook/packages/local-client/src/components/resizable.tsx
+++ jbook/packages/local-client/src/components/resizable.tsx
@@ -4,6 +4,7 @@ import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
+  children?: React.ReactNode;
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
