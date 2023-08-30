15cfc05f3c17e8a4ea53d9b01ab7f5fd749ec7fe
Danny Sarco <dsarco@gmail.com>
2023-08-29 20:18:03 -0400

    More package clean up

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import path from 'path';
-import { createCellsRouter } from './routes/cells';
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
+import { createCellsRouter } from "./routes/cells";
 
 export const serve = (
   port: number,
@@ -16,19 +16,19 @@ export const serve = (
   if (useProxy) {
     app.use(
       createProxyMiddleware({
-        target: 'http://localhost:3000',
+        target: "http://localhost:3000",
         ws: true,
-        logLevel: 'silent',
+        logLevel: "silent",
       })
     );
   } else {
     const packagePath = require.resolve(
-      '@jsnote/local-client/build/index.html'
+      "@my-scrapbook/local-client/build/index.html"
     );
     app.use(express.static(path.dirname(packagePath)));
   }
 
   return new Promise<void>((resolve, reject) => {
-    app.listen(port, resolve).on('error', reject);
+    app.listen(port, resolve).on("error", reject);
   });
 };
