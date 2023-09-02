ad4bd557ffccd40783ee405c31c732c8cfa362c3
Danny Sarco <dsarco@gmail.com>
2023-09-01 20:21:49 -0400

    Added some instructional text

--- jbook/packages/local-client/src/index.tsx
+++ jbook/packages/local-client/src/index.tsx
@@ -1,13 +1,17 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './state';
-import CellList from './components/cell-list';
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./state";
+import CellList from "./components/cell-list";
+import ExplainerText from "./components/example-text";
 
 const App = () => {
   return (
     <Provider store={store}>
+      <div>
+        <ExplainerText />
+      </div>
       <div>
         <CellList />
       </div>
@@ -15,4 +19,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
